ec6e51095e8c8a93c8469d1a392ea1c837fa0915
Anshuman Bisoyi <ansuman746@gmail.com>
2023-01-14 16:03:40 +0530

    tested

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
-app.listen(3000,()=>{
-    console.log("Backend server is listening on port 3000.");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB connection established");
+  })
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Backend server is listening on port 3000.");
 });
