c5d4f6a8fc81dd04de3598d5d8503b6135f46706
Anshuman Bisoyi <ansuman746@gmail.com>
2023-01-14 22:38:29 +0530

    generate jwt accesstoken

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const { response } = require("express");
-//Register
+const jwt = require("jsonwebtoken");
 
+//REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -34,8 +35,16 @@ router.post("/login", async (req, res) => {
     const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
     Originalpassword !== req.body.password &&
       res.status(401).json("Bad credentials");
-    const {password, ...others}=user._doc;
-    res.status(200).json(others);
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      {expiresIn:"3d"}
+    );
+    const { password, ...others } = user._doc;
+    res.status(200).json({...others, accessToken});
   } catch (err) {
     res.status(500).json(err);
   }
