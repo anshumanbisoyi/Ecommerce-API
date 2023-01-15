56258ed943018532717f6a536e483ab2a4436d9a
Anshuman Bisoyi <ansuman746@gmail.com>
2023-01-15 21:15:54 +0530

    added delete user method

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const {
 } = require("./verifyToken");
 const router = require("express").Router();
 
-//Update
+//UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
@@ -27,6 +27,16 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(500).json(err);
   }
 });
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 
 module.exports = router;
